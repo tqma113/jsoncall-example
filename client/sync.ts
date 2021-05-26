9d00c3d26d64c7b4476dbd4b585eb06c155775de
matianqi.tqma <matianqi.tqma@bytedance.com>
2021-05-26 14:42:47 +0800

    update

--- client/sync.ts
+++ client/sync.ts
@@ -1,10 +1,12 @@
 import * as fs from 'fs'
 import path from 'path'
+import { format } from 'prettier'
 import { introspectionClientCodegen } from 'jc-codegen'
 import { call } from '../server'
 
-introspectionClientCodegen(call, {
+introspectionClientCodegen(call, (source) => format(source, {
+  parser: 'typescript',
   semi: false,
   singleQuote: true,
   printWidth: 80,
-}).then(source => fs.writeFileSync(path.resolve(__dirname, './client.ts'), source))
+})).then(source => fs.writeFileSync(path.resolve(__dirname, './client.ts'), source))
